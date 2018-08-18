621993c9fd3576214958a6efde1e75cfc01ec7b8
Denver99 <Denverclaude99@gmail.com>
2018-08-18 13:19:29 +0100

    feat: Adding a ServiceWorker script
    Add a ServiceWorker script to cache requests to all of the site’s assets so that any page that has been visited by a user will be accessible when the user is offline

---
+++ sw.js
@@ -0,0 +1,50 @@
+let urlToCache = [
+					'./', './css/styles.css', 
+					'./css/responsive-index.css', 
+					'./css/responsive-restaurant.css'
+				];
+let cachName = 'mws-restaurant-stage-1-v1';
+let allCaches = [urlToCache, cachName];
+
+/* listen to the install service worker event to cache file*/
+self.addEventListener('install', function(event) {
+	event.waitUntil(
+		caches.open(cachName).then(function(cache) {
+			cache.addAll(urlToCache);
+		})
+	);
+});
+
+/* listen to the activate service worker event to update cache*/
+self.addEventListener('activate', function(event){
+	let cacheWhiteList = Object.values(allCaches);
+	event.waitUntil(
+		caches.keys().then(function(CACHENAME) {
+			return Promise.all(
+				CACHENAME.map(function (cachName) {
+					if (cacheWhiteList.indexOf(cachName) === -1) {
+						return caches.delete(cachName);
+					}
+				})
+			);
+		})
+	);
+})
+
+/* listen to the fetch service worker event to respond or cache a request*/
+self.addEventListener('fetch', function(event){
+	event.respondWith(
+		caches.open(cachName).then(function(cache) {
+			return cache.match(event.request).then(function(response) {
+				if (response) {
+					return response;
+				}
+
+				return fetch(event.request).then(function(new_response){
+					cache.put(event.request, new_response.clone());
+					return new_response;
+				})
+			})
+		})
+	);
+})
