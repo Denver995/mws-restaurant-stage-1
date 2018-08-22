4662c32a1d556bde7ca7bd5a01729d6fb15479c3
Denver99 <Denverclaude99@gmail.com>
2018-08-22 15:57:52 +0100

    test

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `https://denver995/mws-restaurant-stage-1/data/restaurants.json`;
+    return `./data/restaurants.json`;
   }
 
   /**
