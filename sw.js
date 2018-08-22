acf39408d6c5ba11016e68fb0818c9c4b0acee07
Denver99 <Denverclaude99@gmail.com>
2018-08-22 19:43:16 +0100

    test

--- sw.js
+++ sw.js
@@ -1,19 +1,9 @@
 /* store all static file in urlToCache*/
 let urlToCache = [
-					'./css/styles.css', 
+					'/mws-restaurant-stage-1/css/styles.css', 
 					'./css/responsive-index.css', 
 					'./css/responsive-restaurant.css',
-					'./data/restaurants.json',
-					'./img/1.jpg',
-					'./img/2.jpg',
-					'./img/3.jpg',
-					'./img/4.jpg',
-					'./img/5.jpg',
-					'./img/6.jpg',
-					'./img/7.jpg',
-					'./img/8.jpg',
-					'./img/9.jpg',
-					'./img/10.jpg',
+					'./data/restaurants.json'
 					'./js/dbhelper.js',
 					'./js/main.js',
 					'./js/restaurant_info.js',
