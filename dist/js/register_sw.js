00f50b2434a1463b4fa134582b58fb6dc3ff27b4
Denver995 <denverclaude99@gmail.com>
2018-12-10 05:24:40 +0100

    feat: Delete dist directory
    Delete dist directory to allow users get hte latest version of the app when they run the app

--- dist/js/register_sw.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-	navigator.serviceWorker.register('./sw.js', { scope: './'}).then( reg => {
-		console.log('your service worker was successfull registered');
-	}).catch( error => {
-		console.log('failled to registered your service worker', error);
-	});
-}
